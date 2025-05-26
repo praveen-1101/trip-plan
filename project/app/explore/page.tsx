8787ee6d7d9afb872b3f9528369dc2f4a7c810ee
praveen-1101 <borapraveen1101@gmail.com>
2025-05-26 09:21:21 +0530

    done changes

--- project/app/explore/page.tsx
+++ project/app/explore/page.tsx
@@ -2,11 +2,14 @@
 
 import { useSession } from 'next-auth/react';
 import { ExploreContainer } from '@/components/explore/explore-container';
+import { Suspense } from 'react';
 
 export default function ExplorePage() {
   const { data: session } = useSession();
 
   return (
-    <ExploreContainer />
+    <Suspense fallback={<div>Loading...</div>}>
+      <ExploreContainer />
+    </Suspense>
   );
 }
