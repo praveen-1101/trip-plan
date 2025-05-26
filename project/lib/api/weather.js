74762eb7cf2ea504e76e80def43471580502a5ed
praveen-1101 <borapraveen1101@gmail.com>
2025-05-26 14:21:41 +0530

    updated weather api

--- project/lib/api/weather.js
+++ project/lib/api/weather.js
@@ -1,5 +1,5 @@
 
-const API_KEY = process.env.NEXT_PUBLIC_WEATHERAPI_KEY;
+const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
 export async function getWeatherData(lat, lon) {
